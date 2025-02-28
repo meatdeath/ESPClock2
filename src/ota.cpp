c7a2e248569b15e5358a67f21939be518604ace8
meatdeath <vladimir.novac.1980@gmail.com>
2025-02-28 08:52:48 -0500

    Fix OTA

--- src/ota.cpp
+++ src/ota.cpp
@@ -13,8 +13,8 @@ void onOTAProgress(size_t current, size_t final)
     // Log every 1 second
     if (millis() - ota_progress_millis > 1000) {
         ota_progress_millis = millis();
-        Serial.print("\r");
-        Serial.printf("OTA Progress: %u/%u bytes - %u%%    ", current, final, current*100/final);
+        //Serial.print("\r");
+        Serial.printf("OTA Progress: %u/%u bytes - %u%%    \n", current, final, (final)?current*100/final:-1);
     }
 }
 
